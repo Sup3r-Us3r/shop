ed777fc632b3fefea9861b33f7adfd6eb352f910
Sup3r-Us3r <maydersonmello@gmail.com>
2021-02-05 23:24:56 -0300

    updated categories screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
-import 'package:shimmer/shimmer.dart';
+import 'package:dio/dio.dart';
+import 'dart:async';
 import 'package:shop/constants/colors.dart';
 import 'package:shop/models/categories_model.dart';
 import 'package:shop/screens/category_products.dart';
@@ -12,17 +13,70 @@ class Categories extends StatefulWidget {
 }
 
 class _CategoriesState extends State<Categories> {
-  bool isLoading = true;
+  Future<List<CategoryModel>> _getCategories() async {
+    Dio dio = Dio();
+    Response response;
 
-  @override
-  void initState() {
-    super.initState();
+    response = await dio.get('http://192.168.2.8:3333/category/index');
 
-    Future.delayed(Duration(seconds: 2), () {
-      setState(() {
-        isLoading = false;
-      });
-    });
+    var categoriesData = (response.data as List)
+        .map((category) => CategoryModel.fromJson(category))
+        .toList();
+
+    return categoriesData;
+  }
+
+  Widget categoriesList(
+    List<CategoryModel> categoriesData,
+    BuildContext context,
+  ) {
+    return ListView.builder(
+      itemCount: categoriesData.length,
+      itemBuilder: (BuildContext context, int index) {
+        CategoryModel category = categoriesData[index];
+
+        return GestureDetector(
+          onTap: () => Navigator.of(context).pushNamed(
+            '/categoryProducts',
+            arguments: CategoryProducts(
+              categories: categoriesData,
+              categoryIndexTarget: index,
+            ),
+          ),
+          child: Stack(
+            alignment: Alignment.centerLeft,
+            children: [
+              Container(
+                height: 180.0,
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.symmetric(vertical: 20.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20.0),
+                  child: FadeInImage.memoryNetwork(
+                    placeholder: kTransparentImage,
+                    image: category.image.url,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 20.0,
+                child: Text(
+                  category.name,
+                  style: TextStyle(
+                    color: colorWhite,
+                    fontSize: 25.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -40,7 +94,7 @@ class _CategoriesState extends State<Categories> {
           ),
         ),
         title: Text(
-          'Categories',
+          'Categorias',
           style: TextStyle(
             color: colorBlack,
             fontSize: 25.0,
@@ -53,57 +107,16 @@ class _CategoriesState extends State<Categories> {
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 20.0),
-        child: ListView.builder(
-          itemCount: categories.length,
-          itemBuilder: (BuildContext context, int index) {
-            CategoriesModel category = categories[index];
-
-            return GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CategoryProducts(item: index),
-                  ),
-                );
-              },
-              child: Stack(
-                alignment: Alignment.centerLeft,
-                children: [
-                  Container(
-                    height: 180.0,
-                    width: MediaQuery.of(context).size.width,
-                    margin: EdgeInsets.symmetric(vertical: 20.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20.0),
-                      child: isLoading
-                          ? Shimmer.fromColors(
-                              baseColor: Colors.grey[300],
-                              highlightColor: colorWhite,
-                              child: Card(color: colorGray),
-                            )
-                          : Image.network(
-                              category.backgroundUrl,
-                              fit: BoxFit.cover,
-                            ),
-                    ),
-                  ),
-                  Positioned(
-                    left: 20.0,
-                    child: Text(
-                      category.name,
-                      style: TextStyle(
-                        color: colorWhite,
-                        fontSize: 25.0,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            );
+        child: FutureBuilder(
+          future: _getCategories(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return categoriesList(snapshot.data, context);
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
           },
         ),
       ),
